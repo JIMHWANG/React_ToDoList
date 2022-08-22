cd4b95bc47a4b7864b07d5979ca2ce171c9ca685
JIMHWANG <jimhwang1129@gmail.com>
2022-08-22 23:34:16 +0800

    新增 item delete & 完成項目總數

--- js/all.js
+++ js/all.js
@@ -9,9 +9,49 @@ function SortItem(ToDoItemList) {
     return ToDoItemList.length;
 }
 
+function RemoveOneOrAllItem(ToDoItem, Index, setToDoItem) {
+    SortItem(ToDoItem);
+    if (Index === "ALL") {
+        ToDoItem = ToDoItem.filter(item => item.complete !== true);
+    } else {
+        ToDoItem = ToDoItem.filter(item => item.index !== Index);
+    }
+    setToDoItem(ToDoItem);
+    return ToDoItem;
+}
+
+function ToDoListItem({ ToDoItem, setToDoItem, CompleteItemNum, setCompleteItemNum }) {
+    return (
+        <ul className="todoList_item">
+            {
+                ToDoItem.map((item, i) => {
+                    return (
+                        <li key={i}>
+                            <label className="todoList_label">
+                                <input type="checkbox" className="todoList_input" checked={item.complete} onChange={() => {
+                                    item.complete = !item.complete;
+                                    setToDoItem(ToDoItem);
+                                    setCompleteItemNum(ToDoItem.filter(Item => Item.complete === true).length);
+                                }} />
+                                <span>{item.item}</span>
+                            </label>
+                            <a href="#" onClick={() => {
+                                setCompleteItemNum(RemoveOneOrAllItem(ToDoItem, i, setToDoItem).filter(Item => Item.complete === true).length);
+                            }}>
+                                <i className="fa fa-times"></i>
+                            </a>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
 function App() {
     const [ToDoItem, setToDoItem] = useState([]);
     const [NewItem, setNewItem] = useState('');
+    const [CompleteItemNum, setCompleteItemNum] = useState(0);
     return (
         <div>
             <nav>
@@ -20,11 +60,12 @@ function App() {
             <div className="conatiner todoListPage vhContainer">
                 <div className="todoList_Content">
                     <div className="inputBox">
-                        <input type="text" placeholder="請輸入待辦事項" onChange={(e) => {
+                        <input type="text" placeholder="請輸入待辦事項" value={NewItem} onChange={(e) => {
                             setNewItem(e.target.value);
                         }} />
                         <a href="#" onClick={() => {
                             if (NewItem) setToDoItem([...ToDoItem, { item: NewItem, complete: false, index: SortItem(ToDoItem) }]);
+                            setNewItem('');
                         }}>
                             <i className="fa fa-plus"></i>
                         </a>
@@ -36,29 +77,12 @@ function App() {
                             <li><a href="#">已完成</a></li>
                         </ul>
                         <div className="todoList_items">
-                            <ul className="todoList_item">
-                                {
-                                    ToDoItem.map((item, i) => {
-                                        return (
-                                            <li key={i}>
-                                                <label className="todoList_label">
-                                                    <input type="checkbox" className="todoList_input" />
-                                                    <span>{item.item}</span>
-                                                </label>
-                                                <a href="#" onClick={() => {
-                                                    SortItem(ToDoItem);
-                                                    setToDoItem(ToDoItem.filter(item => item.index !== i));
-                                                }}>
-                                                    <i className="fa fa-times"></i>
-                                                </a>
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
+                            <ToDoListItem ToDoItem={ToDoItem} setToDoItem={setToDoItem} CompleteItemNum={CompleteItemNum} setCompleteItemNum={setCompleteItemNum} />
                             <div className="todoList_statistics">
-                                <p> 5 個已完成項目</p>
-                                <a href="#">清除已完成項目</a>
+                                <p> {CompleteItemNum} 個已完成項目</p>
+                                <a href="#" onClick={() => {
+                                    setCompleteItemNum(RemoveOneOrAllItem(ToDoItem, "ALL", setToDoItem).filter(Item => Item.complete === true).length);
+                                }}>清除已完成項目</a>
                             </div>
                         </div>
                     </div>
